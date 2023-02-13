d032bb704407c57df376a5e2a47140ba50ba6afa
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2023-02-13 12:22:18 +0100

    some changes

--- src/components/TeamTestimonial.jsx
+++ src/components/TeamTestimonial.jsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 // icons
+import microsoft from "@/../public/icons/microsoft_logo.png"
+import playCircle from "@/../public/icons/play-circle.png"
 // Images
 import ttImg from "@/../public/ttImg.png"
+// the hover play is a placeholder
 
 export default function TeamTestimonial(){
     return (
@@ -10,23 +13,35 @@ export default function TeamTestimonial(){
                 <h2 className="intro">Trusted by teams that value connections</h2>
                 <p className="sub_text">See how product and engineering teams are using Tango to stay connected and create business opportunities</p>
                 <div className="tmts_content">
-                    <Image alt="" className="rtarrow"/>
+                    <p className="rtarrow">&#8249;</p>
                     <div className="contentCard">
                         <div className="vid_sect">
-                            <Image src={ttImg} alt=""/>
+                            <Image src={ttImg} className="ttImg" alt=""/>
+                            <Image src={playCircle} className="play" alt=""/>
                         </div>
                         <div className="text_sect">
-                            <p className="cpLogo">Microsoft</p>
-                            <p className="testimonial">
-                                “Our team has grown from 250 people to over 700 in the last year, across different officesHollaTags supports our fast growth by giving us a structured place to route people when they have questions. We now have a clear approach to knowledge documentation, distribution and maintenance HollaTags supports our fast growth by giving us a platform for success.”
-                            </p>
+                            <div className="text_div">
+                                <Image src={microsoft} className="cpLogo" alt=""/>
+                                <p className="testimonial">
+                                    “Our team has grown from 250 people to over 700 in the last year, across different officesHollaTags supports our fast growth by giving us a structured place to route people when they have questions. We now have a clear approach to knowledge documentation, distribution and maintenance HollaTags supports our fast growth by giving us a platform for success.”
+                                </p>
+                            </div>
                             <div className="people">
-                                <p className="name">Jared Ukuama,</p>
+                                <h5 className="name">Jared Ukuama,</h5>
                                 <p className="position">Senior DevOps Engineer, Microsoft</p>
                             </div>
                         </div>
                     </div>
-                    <Image alt="" className="ltarrow"/>
+                    <p className="ltarrow">&#8250;</p>
+                </div>
+                <div className="indicator">
+                    <div className="indicators"></div>
+                    <div className="indicators"></div>
+                    <div className="indicators active"></div>
+                    <div className="indicators"></div>
+                    <div className="indicators"></div>
+                    <div className="indicators"></div>
+                    <div className="indicators"></div>
                 </div>
             </div>
         </>
